8c61e0c2601bf17ff182c73f83a98f7c8fb4aee7
Remi Bodin <remi.bodin@gmail.com>
2019-01-19 10:25:00 +0100

    Petit rangement, bouton quit

---
+++ Assets/Scripts/UI/QuitOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class QuitOnClick : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Awake()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener( () => {
+            Application.Quit();
+        });
+    }
+}
